f4115e4d7f71ecdb2d3123dfdfaae209e922417d
DDRIV <ddriv4@gmail.com>
2021-09-06 22:40:59 -0500

    Completed service files for make, model, and sale. Also made interfaces for all three.

--- maad-cars/src/app/app.module.ts
+++ maad-cars/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllSalesComponent } from './all-sales/all-sales.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllSalesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ maad-cars/src/app/sale.ts
@@ -0,0 +1,7 @@
+import { Time } from "@angular/common";
+
+export interface Sale {
+    id: number;
+    time_started: Time;
+    vehicle_id: number
+}
