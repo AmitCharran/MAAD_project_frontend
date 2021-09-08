7f951077ed909721e90eb4c5d8890f368316741a
DDRIV <88058392+DDRIV@users.noreply.github.com>
2021-09-08 15:13:11 -0500

    Capitalized interface name

--- maad-cars/src/app/models/sale.ts
+++ maad-cars/src/app/models/sale.ts
@@ -1,7 +1,7 @@
 import { Time } from "@angular/common";
 import { Vehicle } from "./vehicle";
 
-export interface sale{
+export interface Sale{
     sale_id: number;
     vehicle_id: Vehicle;
     time_started: Time;
