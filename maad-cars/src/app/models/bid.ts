6618bd99eb3080c70dab8b39ea631f4755c8d1d2
Amit <amit.charran@gmail.com>
2021-09-08 14:25:22 -0400

    bid.service.ts functionality added

--- maad-cars/src/app/models/bid.ts
+++ maad-cars/src/app/models/bid.ts
@@ -1,10 +1,11 @@
+import { Time } from "@angular/common";
 import { Sale } from "../sale";
 import { User } from "./user";
 
 export interface bid{
     bid_id:number;
     bid_amount:number;
-    time_stamp:number;
+    time_stamp:Time;
     sale:Sale;
     user:User;
 }
--- maad-cars/src/app/models/sale.ts
+++ maad-cars/src/app/models/sale.ts
@@ -1,8 +1,8 @@
+import { Time } from "@angular/common";
 import { Vehicle } from "./vehicle";
 
 export interface sale{
     sale_id: number;
     vehicle: Vehicle;
-    time_started: number;
-
+    time_started: Time;
 }
