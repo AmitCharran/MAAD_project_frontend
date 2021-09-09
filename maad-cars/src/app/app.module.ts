b9ec1acec1c2a17bb6c0fffee6c300cd662757a8
Minh Thuy Nguyen <nguyentranminhthuy96@gmail.com>
2021-09-08 19:05:19 -0700

    added current progress on vehicles

--- maad-cars/src/app/app.module.ts
+++ maad-cars/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
-import { CreateVehicleComponent } from './components/create-vehicle/create-vehicle.component';
 import { AllSalesComponent } from './components/all-sales/all-sales.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { SaleDetailComponent } from './components/sale-detail/sale-detail.component';
 import { MySalesComponent } from './components/my-sales/my-sales.component';
 import { BidsOnCarComponent } from './components/bids-on-car/bids-on-car.component';
-import { FormsModule } from '@angular/forms';
+import { VehicleListComponent } from './components/vehicles/vehicle-list/vehicle-list.component';
+import { CreateVehicleComponent } from './components/vehicles/create-vehicle/create-vehicle.component';
+import { CreateVehicleInputMakeComponent } from './components/vehicles/create-vehicle-input-make/create-vehicle-input-make.component';
+import { CreateVehicleInputModelComponent } from './components/vehicles/create-vehicle-input-model/create-vehicle-input-model.component';
+import { CreateVehicleFormComponent } from './components/vehicles/create-vehicle-form/create-vehicle-form.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { FormsModule } from '@angular/forms';
     SaleDetailComponent,
     VehicleListComponent,
     CreateVehicleComponent,
+    CreateVehicleInputMakeComponent,
+    CreateVehicleInputModelComponent,
+    CreateVehicleFormComponent,
     MySalesComponent,
     BidsOnCarComponent
   ],
